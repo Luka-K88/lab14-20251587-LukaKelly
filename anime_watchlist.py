2ed2213c52f074adc3445fb6f0f131f37d49dc11
Luka Kelly <lukakelly001@gmail.com>
2026-06-03 10:28:03 +1200

    Added Anime Watchlist Improvement (Rating out of 5)

--- anime_watchlist.py
+++ anime_watchlist.py
@@ -1,22 +1,24 @@
 class Anime:
-    def __init__(self, title, genre, status):
+    def __init__(self, title, genre, status, rating):
         self.title = title
         self.genre = genre
         self.status = status
+        self.rating = rating
     
     def displayInfo(self):
         print("=== Watchlist ===")
         print(f"Title: {self.title.title()}")
         print(f"Genre: {self.genre.title()}")
         print(f"Status: {self.status.title()}")
+        print(f"Rating: {self.rating.title()}")
 
     def updateStatus(self):
         self.status = input("Enter 'Watched', 'Watching', or 'Waiting': ").lower()
 
 
-anime1 = Anime("Demon Slayer", "Action", "Watching")
-anime2 = Anime("Spy x Family", "Comedy", "Completed")
-anime3 = Anime("Haikyuu", "Sports", "Waiting")
+anime1 = Anime("Demon Slayer", "Action", "Watching", "3/5")
+anime2 = Anime("Spy x Family", "Comedy", "Completed", "3/5")
+anime3 = Anime("Haikyuu", "Sports", "Waiting", "3/5")
 
 anime_list = [anime1, anime2, anime3]
 
